e2c56959e6ae8b750c73a4570eb949bd4cda927e
kayc0des <k.boafo@alustudent.com>
2025-02-22 14:51:49 +0200

    mobile app repo updated and structured

---
+++ animaltrackid_mobile/lib/features/reusables/custom_appbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+  // Reuseable arguments
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        // build
+        );
+  }
+}
---
+++ animaltrackid_mobile/lib/utils/constants/icons.dart
@@ -0,0 +1,11 @@
+// App icons
+
+class AppIcons {
+  static const String homeIcon = 'assets/icons/home_icon.svg';
+  static const String trackIcon = 'assets/icons/track_icon.svg';
+  static const String profileIcon = 'assets/icons/user_icon.svg';
+  static const String submitIcon = 'assets/icons/submit_icon.svg';
+  static const String arrowLeft = 'assets/icons/arrow_left.svg';
+  static const String arrowRight = 'assets/icons/arrow_right.svg';
+  static const String infoIcon = 'assets/icons/info_icon.svg';
+}
