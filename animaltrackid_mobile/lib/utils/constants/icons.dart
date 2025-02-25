8bb3438a0c12d7af85ed90557d26f03f2f534446
kayc0des <k.boafo@alustudent.com>
2025-02-25 14:07:56 +0200

    more icons

--- animaltrackid_mobile/lib/utils/constants/icons.dart
+++ animaltrackid_mobile/lib/utils/constants/icons.dart
@@ -4,8 +4,11 @@ class AppIcons {
   static const String homeIcon = 'assets/icons/home_icon.svg';
   static const String trackIcon = 'assets/icons/track_icon.svg';
   static const String profileIcon = 'assets/icons/user_icon.svg';
+  static const String menuIcon = 'assets/icons/menu_icon.svg';
   static const String submitIcon = 'assets/icons/submit_icon.svg';
   static const String arrowLeft = 'assets/icons/arrow_left.svg';
-  static const String arrowRight = 'assets/icons/arrow_right.svg';
+  static const String arrowRight = 'assets/icons/arrowRight.svg';
   static const String infoIcon = 'assets/icons/info_icon.svg';
+  static const String hideIcon = 'assets/icons/eye_icon.svg';
+  static const String showIcon = 'assets/icons/open_eye.svg';
 }
