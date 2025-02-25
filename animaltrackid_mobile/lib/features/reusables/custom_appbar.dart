65edfb6b4a6c4c6852f4b6c8c3a34d44da4e1138
kayc0des <k.boafo@alustudent.com>
2025-02-25 15:44:19 +0200

    appbars

--- animaltrackid_mobile/lib/features/reusables/custom_appbar.dart
+++ animaltrackid_mobile/lib/features/reusables/custom_appbar.dart
@@ -5,6 +5,7 @@ import '../../utils/constants/colors.dart';
 import '../../utils/constants/fonts.dart';
 import '../../utils/constants/icons.dart';
 
+//------- Onboarding App Bar------//
 class OnboardingAppBar extends StatelessWidget implements PreferredSizeWidget {
   final VoidCallback onSkipPressed;
 
@@ -47,6 +48,92 @@ class OnboardingAppBar extends StatelessWidget implements PreferredSizeWidget {
               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
               minimumSize: const Size(0, 0),
             ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+//------- Return App Bar------//
+class ReturnAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback onBackPressed;
+
+  const ReturnAppBar({super.key, required this.onBackPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: IconButton(
+        onPressed: onBackPressed,
+        icon: SvgPicture.asset(
+          AppIcons.arrowLeft,
+          width: 24,
+          height: 24,
+          colorFilter:
+              const ColorFilter.mode(AppColors.textColor, BlendMode.srcIn),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+//------- Home App Bar------//
+class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback onBackPressed;
+
+  const HomeAppBar({super.key, required this.onBackPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: IconButton(
+        onPressed: onBackPressed,
+        icon: SvgPicture.asset(
+          AppIcons.menuIcon,
+          width: 24,
+          height: 24,
+          colorFilter:
+              const ColorFilter.mode(AppColors.primaryColor, BlendMode.srcIn),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+//------- Info App Bar------//
+class InfoAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback onInfoPressed;
+
+  const InfoAppBar({super.key, required this.onInfoPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      actions: [
+        IconButton(
+          onPressed: onInfoPressed,
+          icon: SvgPicture.asset(
+            AppIcons.infoIcon,
+            width: 24,
+            height: 24,
+            colorFilter:
+                const ColorFilter.mode(AppColors.textColor, BlendMode.srcIn),
           ),
         ),
       ],
